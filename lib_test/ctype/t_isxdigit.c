bb3c05ee454354b7b2346331ad88594eb186024b
Song Hui <song.hui.sie@gmail.com>
2015-01-21 17:10:50 -0800

    correct input error

--- lib_test/ctype/t_isxdigit.c
+++ lib_test/ctype/t_isxdigit.c
@@ -3,24 +3,24 @@
 
 int main()
 {
-	int var1[] = "tuts";
-	int var2[] = "0xE";
+	char var1[] = "tuts";
+	char var2[] = "0xE";
 
 	if(isxdigit(var1[0]))
 	{
-		printf("var1 = |%c| is a hexadecimal character\n", var1);
+		printf("var1 = |%s| is a hexadecimal character\n", var1);
 	}
 	else
 	{
-		printf("var1 = |%c| is not a hexadecimal character\n", var1);
+		printf("var1 = |%s| is not a hexadecimal character\n", var1);
 	}
 	if(isxdigit(var2[0]))
 	{
-		printf("var2 = |%c| is a hexadecimal character\n", var2);
+		printf("var2 = |%s| is a hexadecimal character\n", var2);
 	}
 	else
 	{
-		printf("var2 = |%c| is not a hexadecimal character\n", var2);
+		printf("var2 = |%s| is not a hexadecimal character\n", var2);
 	}
 		
 	
