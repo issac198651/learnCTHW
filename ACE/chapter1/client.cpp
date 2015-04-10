a78b01373c6db64c4e134ff46e01a365402fa5e5
songHu <song.hui.sie@gmail.com>
2015-04-10 23:17:53 +0800

    add simple svr and client

---
+++ ACE/chapter1/client.cpp
@@ -0,0 +1,20 @@
+int ACE_MAIN(int, ACE_TCHAR*[])
+{
+	int ret = 0;
+	const char *ip = "127.0.0.1";
+	short port = 60000;
+	ACE_INET_Addr srvr(port, ip);
+	ACE_SOCK_Connector connector;
+	ACE_SOCK_Stream peer;
+
+	//ACE_Time_Value time(0, 500);
+	//ret = connector.connect(peer, srvr, &time);
+	ret = connector.connect(peer, srvr);
+	if(ret == -1)
+	{
+		ACE_ERROR_RETURN((LM_ERROR, ACE_TEXT("%p\n"), ACE_TEXT("connect")), -1);
+	}
+	peer.send_n("hello world!", 12);
+	peer.close();
+	return(0);
+}
